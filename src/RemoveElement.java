cb0a4a2b96dd3e45abc00ac7b15650abe4f3d557
ChiaYi Huang <jyhuangtc@gmail.com>
2019-06-09 10:11:01 +0800

    No.27 Remove Element

---
+++ src/RemoveElement.java
@@ -0,0 +1,89 @@
+public class RemoveElement {
+
+    /**
+
+        Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+
+    Example 1:
+
+    Given nums = [3,2,2,3], val = 3,
+
+    Your function should return length = 2, with the first two elements of nums being 2.
+
+    It doesn't matter what you leave beyond the returned length.
+
+
+    Example 2:
+
+    Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+    Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+    Note that the order of those five elements can be arbitrary.
+
+    It doesn't matter what values are set beyond the returned length.
+
+
+    Clarification:
+
+    Confused why the returned value is an integer but your answer is an array?
+
+    Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+    Internally you can think of this:
+
+    // nums is passed in by reference. (i.e., without making a copy)
+    int len = removeElement(nums, val);
+
+    // any modification to nums in your function would be known by the caller.
+    // using the length returned by your function, it prints the first len elements.
+    for (int i = 0; i < len; i++) {
+        print(nums[i]);
+    }
+     */
+
+    //20190608 Solution 1
+    public int removeElement(int[] nums, int val) {
+        int removeElementCount = 0;
+        int index = 0;
+        while (index < nums.length-removeElementCount) {
+            if (val == nums[index]) {
+                for (int i = index; i < nums.length-1; i++) {
+                    nums[i] = nums[i+1];
+                }
+                removeElementCount++;
+            } else {
+                index++;
+            }
+        }
+        return index;
+    }
+
+    //20190608 
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement re = new RemoveElement();
+        int[] nums = {3,2,2,3};
+        int val = 3;
+//        int[] nums = {0,1,2,2,3,0,4,2};
+//        int val = 2;
+        int output = re.removeElement(nums, val);
+        System.out.println(output);
+    }
+
+}
